20eca319ca33a8c883a9e03815d37f1012f4272a
SaiNithin3470 <nithinpersonal158@gmail.com>
2026-07-15 11:15:00 +0530

    Time: 23 ms (5.49%), Space: 23.1 MB (5.04%) - LeetHub

---
+++ 0012-integer-to-roman/0012-integer-to-roman.cpp
@@ -0,0 +1,69 @@
+class Solution {
+public:
+    string intToRoman(int num) {
+        map<int,string>map;
+        string ans="";
+        map[1]="I";
+        map[2]="II";
+        map[3]="III";
+        map[4]="IV";
+        map[6]="VI";
+        map[7]="VII";
+        map[8]="VIII";
+        map[9]="IX";
+        map[5]="V";
+        map[10]="X";
+        map[60]="LX";
+        map[70]="LXX";
+        map[80]="LXXX";
+        map[50]="L";
+        map[100]="C";
+        map[200]="CC";
+        map[300]="CCC";
+        map[600]="DC";
+        map[700]="DCC";
+        map[800]="DCCC";
+
+        map[500]="D";
+        map[1000]="M";
+        map[40]="XL";
+        map[90]="XC";
+        map[400]="CD";
+        map[900]="CM";
+        
+        int m=1000;
+        int l;
+        while(m>0 && num>0)
+        {
+            if((num/m)!=0)
+            {
+                l=num/m;
+                if(map.count(l*m)==1)
+                {
+            
+                  ans+=map[l*m];
+                 
+                }
+                else{
+                    while(l>0)
+                    {
+                       ans+=map[m];
+                       l--;
+                    }
+                }
+                 num=num%m;
+                    
+            }
+            m=m/10;
+           
+
+
+        }
+        return ans;
+
+
+
+        
+        
+    }
+};
